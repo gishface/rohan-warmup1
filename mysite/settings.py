75bb97d7dde293824e8036836a4f031896b28b9a
Rohan Ramakrishnan <rohan.ramakrishnan@berkeley.edu>
2013-02-15 23:00:16 -0800

    my django app 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -105,7 +105,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'C:\Users\Rohan\Desktop\cs169\mysite\mysite\mtemplates'
+	'C:/Users/Rohan/Desktop/cs169/warmupproj/mysite/mtemplates'
 )
 
 INSTALLED_APPS = (
